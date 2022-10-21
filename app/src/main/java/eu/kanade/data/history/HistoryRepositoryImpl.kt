c942f2a190b926752f27cb3b8ab9cc5f0a85fc1c
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-10-21 21:06:12 +0200

    refactoring, fixing logic, adding filter logic (partial)

--- app/src/main/java/eu/kanade/data/history/HistoryRepositoryImpl.kt
+++ app/src/main/java/eu/kanade/data/history/HistoryRepositoryImpl.kt
@@ -8,6 +8,7 @@ import eu.kanade.domain.history.model.HistoryUpdate
 import eu.kanade.domain.history.model.HistoryWithRelations
 import eu.kanade.domain.history.repository.HistoryRepository
 import eu.kanade.domain.manga.model.Manga
+import eu.kanade.domain.manga.model.TriStateFilter
 import eu.kanade.tachiyomi.util.system.logcat
 import kotlinx.coroutines.flow.Flow
 import logcat.LogPriority
@@ -31,18 +32,55 @@ class HistoryRepositoryImpl(
     override suspend fun getNextChapter(mangaId: Long): Chapter? {
         val manga = handler.awaitOne { mangasQueries.getMangaById(mangaId, mangaMapper) }
 
-        val sortFunction: (Chapter, Chapter) -> Int = when (manga.sorting) {
-            Manga.CHAPTER_SORTING_SOURCE -> { c1, c2 -> c2.sourceOrder.compareTo(c1.sourceOrder) }
-            Manga.CHAPTER_SORTING_NUMBER -> { c1, c2 -> c1.chapterNumber.compareTo(c2.chapterNumber) }
-            Manga.CHAPTER_SORTING_UPLOAD_DATE -> { c1, c2 -> c1.dateUpload.compareTo(c2.dateUpload) }
-            else -> throw NotImplementedError("Unknown sorting method")
+        val chapters = handler.awaitList { chaptersQueries.getChaptersByMangaId(mangaId, chapterMapper) }
+            .applyFilters(manga)
+            .sortedWith(sortFunction(manga))
+
+        return chapters.find { !it.read }
+    }
+
+    private fun List<Chapter>.applyFilters(manga: Manga): Sequence<Chapter> {
+        // val isLocalManga = manga.isLocal()
+        val unreadFilter = manga.unreadFilter
+        // val downloadedFilter = manga.downloadedFilter
+        val bookmarkedFilter = manga.bookmarkedFilter
+
+        return asSequence()
+            .filter { chapter ->
+                when (unreadFilter) {
+                    TriStateFilter.DISABLED -> true
+                    TriStateFilter.ENABLED_IS -> !chapter.read
+                    TriStateFilter.ENABLED_NOT -> chapter.read
+                }
+            }
+            .filter { chapter ->
+                when (bookmarkedFilter) {
+                    TriStateFilter.DISABLED -> true
+                    TriStateFilter.ENABLED_IS -> chapter.bookmark
+                    TriStateFilter.ENABLED_NOT -> !chapter.bookmark
+                }
+            }
+            /* TODO implement download filter
+            .filter { chapter ->
+                when (downloadedFilter) {
+                    TriStateFilter.DISABLED -> true
+                    TriStateFilter.ENABLED_IS -> chapter.isDownloaded || isLocalManga
+                    TriStateFilter.ENABLED_NOT -> !chapter.isDownloaded && !isLocalManga
+                }
+            }
+             */
+    }
+
+    override suspend fun getNextChapter(mangaId: Long, chapterId: Long): Chapter? {
+        val chapter = handler.awaitOne { chaptersQueries.getChapterById(chapterId, chapterMapper) }
+        val manga = handler.awaitOne { mangasQueries.getMangaById(mangaId, mangaMapper) }
+
+        if (!chapter.read) {
+            return chapter
         }
 
         val chapters = handler.awaitList { chaptersQueries.getChaptersByMangaId(mangaId, chapterMapper) }
-            .sortedWith(sortFunction)
-
-        val firstUnreadChapter = chapters.find { !it.read } ?: return null
-        val chapter = chapters.getOrElse(chapters.indexOf(firstUnreadChapter) - 1) { return null }
+            .sortedWith(sortFunction(manga))
 
         val currChapterIndex = chapters.indexOfFirst { chapter.id == it.id }
         return when (manga.sorting) {
@@ -65,41 +103,11 @@ class HistoryRepositoryImpl(
         }
     }
 
-    override suspend fun getNextChapter(mangaId: Long, chapterId: Long): Chapter? {
-        val chapter = handler.awaitOne { chaptersQueries.getChapterById(chapterId, chapterMapper) }
-        val manga = handler.awaitOne { mangasQueries.getMangaById(mangaId, mangaMapper) }
-
-        if (!chapter.read) {
-            return chapter
-        }
-
-        val sortFunction: (Chapter, Chapter) -> Int = when (manga.sorting) {
-            Manga.CHAPTER_SORTING_SOURCE -> { c1, c2 -> c2.sourceOrder.compareTo(c1.sourceOrder) }
-            Manga.CHAPTER_SORTING_NUMBER -> { c1, c2 -> c1.chapterNumber.compareTo(c2.chapterNumber) }
-            Manga.CHAPTER_SORTING_UPLOAD_DATE -> { c1, c2 -> c1.dateUpload.compareTo(c2.dateUpload) }
-            else -> throw NotImplementedError("Unknown sorting method")
-        }
-
-        val chapters = handler.awaitList { chaptersQueries.getChaptersByMangaId(mangaId, chapterMapper) }
-            .sortedWith(sortFunction)
-
-        val currChapterIndex = chapters.indexOfFirst { chapter.id == it.id }
-        return when (manga.sorting) {
-            Manga.CHAPTER_SORTING_SOURCE -> chapters.getOrNull(currChapterIndex + 1)
-            Manga.CHAPTER_SORTING_NUMBER -> {
-                val chapterNumber = chapter.chapterNumber
-
-                ((currChapterIndex + 1) until chapters.size)
-                    .map { chapters[it] }
-                    .firstOrNull {
-                        it.chapterNumber > chapterNumber &&
-                            it.chapterNumber <= chapterNumber + 1
-                    }
-            }
-            Manga.CHAPTER_SORTING_UPLOAD_DATE -> {
-                chapters.drop(currChapterIndex + 1)
-                    .firstOrNull { it.dateUpload >= chapter.dateUpload }
-            }
+    private fun sortFunction(manga: Manga) = Comparator<Chapter> { c1, c2 ->
+        when (manga.sorting) {
+            Manga.CHAPTER_SORTING_SOURCE -> { c2.sourceOrder.compareTo(c1.sourceOrder) }
+            Manga.CHAPTER_SORTING_NUMBER -> { c1.chapterNumber.compareTo(c2.chapterNumber) }
+            Manga.CHAPTER_SORTING_UPLOAD_DATE -> { c1.dateUpload.compareTo(c2.dateUpload) }
             else -> throw NotImplementedError("Unknown sorting method")
         }
     }
