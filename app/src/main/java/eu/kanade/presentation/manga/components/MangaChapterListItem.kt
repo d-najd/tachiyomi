6196d7994194d497efb496bc334f1d3ac2ea0f40
Deltashot <dimitar.hristijan.najdovski@gmail.com>
2022-11-04 20:54:35 +0100

    basic animation working

--- app/src/main/java/eu/kanade/presentation/manga/components/MangaChapterListItem.kt
+++ app/src/main/java/eu/kanade/presentation/manga/components/MangaChapterListItem.kt
@@ -1,15 +1,23 @@
 package eu.kanade.presentation.manga.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.DismissDirection
+import androidx.compose.material.DismissValue
+import androidx.compose.material.SwipeToDismiss
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.rememberDismissState
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
@@ -22,6 +30,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -52,89 +61,133 @@ fun MangaChapterListItem(
     onClick: () -> Unit,
     onDownloadClick: ((ChapterDownloadAction) -> Unit)?,
 ) {
-    Row(
-        modifier = modifier
-            .selectedBackground(selected)
-            .combinedClickable(
-                onClick = onClick,
-                onLongClick = onLongClick,
-            )
-            .padding(start = 16.dp, top = 12.dp, end = 8.dp, bottom = 12.dp),
-    ) {
-        Column(modifier = Modifier.weight(1f)) {
-            val textColor = if (bookmark && !read) {
-                MaterialTheme.colorScheme.primary
-            } else {
-                MaterialTheme.colorScheme.onSurface
+    val state = rememberDismissState(
+        confirmStateChange = {
+            if (it == DismissValue.DismissedToStart) {
+                // items.remove(item)
             }
-            val textAlpha = remember(read) { if (read) ReadItemAlpha else 1f }
-            val textSubtitleAlpha = remember(read) { if (read) ReadItemAlpha else SecondaryItemAlpha }
+            true
+        },
+    )
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                var textHeight by remember { mutableStateOf(0) }
-                if (bookmark) {
-                    Icon(
-                        imageVector = Icons.Filled.Bookmark,
-                        contentDescription = stringResource(R.string.action_filter_bookmarked),
-                        modifier = Modifier
-                            .sizeIn(maxHeight = with(LocalDensity.current) { textHeight.toDp() - 2.dp }),
-                        tint = MaterialTheme.colorScheme.primary,
-                    )
-                    Spacer(modifier = Modifier.width(2.dp))
-                }
-                Text(
-                    text = title,
-                    color = textColor,
-                    style = MaterialTheme.typography.bodyMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis,
-                    onTextLayout = { textHeight = it.size.height },
-                    modifier = Modifier.alpha(textAlpha),
+    SwipeToDismiss(
+        state = state,
+        background = {
+            /*
+            val color = when (state.dismissDirection){
+                DismissDirection.EndToStart -> Color.Red,
+                    DismissDirection.StartToEnd -> Color.Black,
+                    null -> Color.Blue
+            }
+             */
+            val color = Color.Blue
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color)
+                    .padding(8.dp),
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.align(Alignment.CenterEnd),
                 )
             }
-            Spacer(modifier = Modifier.height(6.dp))
-            Row(modifier = Modifier.alpha(textSubtitleAlpha)) {
-                ProvideTextStyle(
-                    value = MaterialTheme.typography.bodyMedium
-                        .copy(color = textColor, fontSize = 12.sp),
-                ) {
-                    if (date != null) {
-                        Text(
-                            text = date,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
-                        if (readProgress != null || scanlator != null) DotSeparatorText()
+        },
+        dismissContent = {
+            Row(
+                modifier = modifier
+                    .selectedBackground(selected)
+                    .combinedClickable(
+                        onClick = onClick,
+                        onLongClick = onLongClick,
+                    )
+                    .background(Color.Black)
+                    .padding(start = 16.dp, top = 12.dp, end = 8.dp, bottom = 12.dp),
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    val textColor = if (bookmark && !read) {
+                        MaterialTheme.colorScheme.primary
+                    } else {
+                        MaterialTheme.colorScheme.onSurface
                     }
-                    if (readProgress != null) {
+                    val textAlpha = remember(read) { if (read) ReadItemAlpha else 1f }
+                    val textSubtitleAlpha = remember(read) { if (read) ReadItemAlpha else SecondaryItemAlpha }
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        var textHeight by remember { mutableStateOf(0) }
+                        if (bookmark) {
+                            Icon(
+                                imageVector = Icons.Filled.Bookmark,
+                                contentDescription = stringResource(R.string.action_filter_bookmarked),
+                                modifier = Modifier
+                                    .sizeIn(maxHeight = with(LocalDensity.current) { textHeight.toDp() - 2.dp }),
+                                tint = MaterialTheme.colorScheme.primary,
+                            )
+                            Spacer(modifier = Modifier.width(2.dp))
+                        }
                         Text(
-                            text = readProgress,
+                            text = title,
+                            color = textColor,
+                            style = MaterialTheme.typography.bodyMedium,
                             maxLines = 1,
                             overflow = TextOverflow.Ellipsis,
-                            modifier = Modifier.alpha(ReadItemAlpha),
+                            onTextLayout = { textHeight = it.size.height },
+                            modifier = Modifier.alpha(textAlpha),
                         )
-                        if (scanlator != null) DotSeparatorText()
                     }
-                    if (scanlator != null) {
-                        Text(
-                            text = scanlator,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                        )
+                    Spacer(modifier = Modifier.height(6.dp))
+                    Row(modifier = Modifier.alpha(textSubtitleAlpha)) {
+                        ProvideTextStyle(
+                            value = MaterialTheme.typography.bodyMedium
+                                .copy(color = textColor, fontSize = 12.sp),
+                        ) {
+                            if (date != null) {
+                                Text(
+                                    text = date,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                )
+                                if (readProgress != null || scanlator != null) DotSeparatorText()
+                            }
+                            if (readProgress != null) {
+                                Text(
+                                    text = readProgress,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                    modifier = Modifier.alpha(ReadItemAlpha),
+                                )
+                                if (scanlator != null) DotSeparatorText()
+                            }
+                            if (scanlator != null) {
+                                Text(
+                                    text = scanlator,
+                                    maxLines = 1,
+                                    overflow = TextOverflow.Ellipsis,
+                                )
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-        // Download view
-        if (onDownloadClick != null) {
-            ChapterDownloadIndicator(
-                enabled = downloadIndicatorEnabled,
-                modifier = Modifier.padding(start = 4.dp),
-                downloadStateProvider = downloadStateProvider,
-                downloadProgressProvider = downloadProgressProvider,
-                onClick = onDownloadClick,
-            )
-        }
-    }
+                // Download view
+                if (onDownloadClick != null) {
+                    ChapterDownloadIndicator(
+                        enabled = downloadIndicatorEnabled,
+                        modifier = Modifier.padding(start = 4.dp),
+                        downloadStateProvider = downloadStateProvider,
+                        downloadProgressProvider = downloadProgressProvider,
+                        onClick = onDownloadClick,
+                    )
+                }
+            }
+        },
+        directions = setOf(DismissDirection.EndToStart),
+    )
+}
+
+@Composable
+private fun MangaChapterListItemContent() {
 }
